a855a481c732906e9895945016495a98a4f7df7d
conrad <awodun42@gmail.com>
2025-09-06 02:41:24 -0700

    Update  production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,66 +1,48 @@
- import express from "express";
-import dotenv from "dotenv";
+import express from "express";
 import cors from "cors";
-import path from "path"
+import dotenv from "dotenv";
+import path from "path";
 
-import noteRoutes from "./routes/noteRoutes.js";
+import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-
 dotenv.config();
 
-
-
- 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-
-// Middleware to parse JSON request bodies
-
-if(process.env.NODE_ENV !== "production") {
-
-app.use(
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-    origin: "http://localhost:5173",
-    "https://api-learnboard.onrender.com"
-})
-);
-
+      origin: "http://localhost:5173",
+    })
+  );
 }
-
-
-app.use (express.json()); // this middleeware will parse JSON bodies: req.body
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
-// Custom middleware to log request method and URL
 
+// our simple custom middleware
 // app.use((req, res, next) => {
-//    console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//    next();
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
 // });
 
+app.use("/api/notes", notesRoutes);
 
-
-
-
-
-
-app.use("/api/notes", noteRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../fontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"../fontend", "dist", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-// Connect to the database and start the server
-connectDB().then(() =>{
-app.listen(PORT, ()  => {
-    console.log ("server started on PORT:", PORT);
-}); 
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
 
