ae92deb29212d6efa323c00280fd35a5b99a4bdc
conrad <awodun42@gmail.com>
2025-09-06 04:48:04 -0700

    Update  production

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../fontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../fontend", "dist", "index.html"));
